212c27865748ac8eb4abcf2dd2a61d7137010fa9
lethnis <arkady.shu@gmail.com>
2026-02-12 15:24:31 +0700

    feat(notebooks): added refine-agent and langfuse api tests

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,5 +1,4 @@
 import os
-from datetime import date
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
@@ -11,7 +10,7 @@ llm = ChatOpenAI(
     api_key=os.getenv("OPENAI_API_KEY"),
     base_url=os.getenv("OPENAI_BASE_URL"),
     model_name=os.getenv("OPENAI_REFINE_MODEL"),
-    timeout=30,
+    timeout=60,
     max_retries=3
 )
 
@@ -19,13 +18,13 @@ structured_model = ChatOpenAI(
     api_key=os.getenv("OPENAI_API_KEY"),
     base_url=os.getenv("OPENAI_BASE_URL"),
     model_name=os.getenv("OPENAI_STRUCTURED_MODEL"),
-    timeout=30,
+    timeout=60,
     max_retries=3
 )
 
 class ActInfo(BaseModel):
     full_name: str | None = Field(default=None, description="Полное наименование правового акта (тип + наименование государственного органа)")
-    publication_date: date | None = Field(default=None, description="Дата опубликования документа")
+    publication_date: str | None = Field(default=None, description="Дата опубликования документа")
     number: str | None = Field(default=None, description="Номер документа (например, 3789-р)")
     title: str | None = Field(default=None, description="Заголовок документа")
     government_agency_name: str | None = Field(default=None, description="Наименование государственного органа")
