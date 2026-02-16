608fca37352c21c3a0c54e2286122a56fa4c463a
Home <arkad.sh@yandex.ru>
2026-02-16 14:54:43 +0700

    feat(data): added code to fill langfuse with RusLawOD dataset from notebook 4.0-langfuse-create-and-eval-dataset.ipynb

--- src/data/fill_langfuse.py
+++ src/data/fill_langfuse.py
@@ -4,39 +4,41 @@ from tqdm import tqdm
 
 from src.constants import ActInfo
 
-client = get_client()
-dataset = load_from_disk("../data/raw")
-client.create_dataset(name="RusLawOD/train", expected_output_schema=ActInfo.model_json_schema())
-client.create_dataset(name="RusLawOD/test", expected_output_schema=ActInfo.model_json_schema())
+if __name__ == "__main__":
 
-for item in tqdm(dataset["train"], desc="Loading train dataset..."):
+    client = get_client()
+    dataset = load_from_disk("../data/raw")
+    client.create_dataset(name="RusLawOD/train", expected_output_schema=ActInfo.model_json_schema())
+    client.create_dataset(name="RusLawOD/test", expected_output_schema=ActInfo.model_json_schema())
 
-    client.create_dataset_item(
-        dataset_name="RusLawOD/train",
-        input=item["text"],
-        expected_output={
-            "full_name": item["full_name"],
-            "publication_date": item["publication_date"],
-            "number": item["number"],
-            "title": item["title"],
-            "government_agency_name": item["government_agency_name"],
-            "signatory": item["signatory"],
-            "type": item["type"],
-        },
-    )
+    for item in tqdm(dataset["train"], desc="Loading train dataset..."):
 
-for item in tqdm(dataset["test"], desc="Loading test dataset..."):
+        client.create_dataset_item(
+            dataset_name="RusLawOD/train",
+            input=item["text"],
+            expected_output={
+                "full_name": item["full_name"],
+                "publication_date": item["publication_date"],
+                "number": item["number"],
+                "title": item["title"],
+                "government_agency_name": item["government_agency_name"],
+                "signatory": item["signatory"],
+                "type": item["type"],
+            },
+        )
 
-    client.create_dataset_item(
-        dataset_name="RusLawOD/test",
-        input=item["text"],
-        expected_output={
-            "full_name": item["full_name"],
-            "publication_date": item["publication_date"],
-            "number": item["number"],
-            "title": item["title"],
-            "government_agency_name": item["government_agency_name"],
-            "signatory": item["signatory"],
-            "type": item["type"],
-        },
-    )
+    for item in tqdm(dataset["test"], desc="Loading test dataset..."):
+
+        client.create_dataset_item(
+            dataset_name="RusLawOD/test",
+            input=item["text"],
+            expected_output={
+                "full_name": item["full_name"],
+                "publication_date": item["publication_date"],
+                "number": item["number"],
+                "title": item["title"],
+                "government_agency_name": item["government_agency_name"],
+                "signatory": item["signatory"],
+                "type": item["type"],
+            },
+        )
